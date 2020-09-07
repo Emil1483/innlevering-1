93696637c03edd67c169a2150c4c6d21bacd2186
Emil Djupvik <emil14833@gmail.com>
2020-09-07 21:43:00 +0200

    use realistic groth

--- oppgave-2-d.py
+++ oppgave-2-d.py
@@ -5,20 +5,26 @@ import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def exponential_growth(start, rate, duration):
+def realistic_growth(start, rate, duration, limit, dt):
     current = start
 
     time = [0]
     population = [current]
-    for current_time in range(duration):
-        current += start * math.log(rate) * rate**current_time
+
+    current_time = 0
+    while current_time <= duration:
+        derivedive = start * math.log(rate) * rate**current_time
+        current += dt * derivedive * (1 - current / limit)
 
         population.append(current)
-        time.append(current_time + 1)
+        time.append(current_time)
+
+        current_time += dt
 
     return time, population
 
 
-time, population = exponential_growth(1000, 1.62, 15)
+time, population = realistic_growth(1000, 1.62, 15, 75000, 0.001)
 pylab.plot(time, population)
+print(population[-1])
 # %%
